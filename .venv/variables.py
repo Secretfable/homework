1a9111694c41530c880d03dd6904c0b20a355caf
Secretfable <81475972+Secretfable@users.noreply.github.com>
2024-11-21 19:37:35 +0300

    made practise for lesson

---
+++ .venv/variables.py
@@ -0,0 +1,6 @@
+homework_done = 12
+hours_spended = 1.5
+name_of_course = 'Python'
+hours_per_tusk = hours_spended/homework_done
+print('Курс:',name_of_course,', всего задач:',homework_done, ', затрачено часов:',
+hours_spended,', среднее время выполнения:', hours_per_tusk ,'часа.')
